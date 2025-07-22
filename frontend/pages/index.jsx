efe21c94ea5ca3195825069164cfea8c6e1c2ced
HerbKingz <highgradeclub420@gmail.com>
2025-07-22 18:17:11 +0100

    Create index.jsx

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="p-8">
+        <h1 className="text-4xl font-bold">Welcome to Herb Kingz</h1>
+        <p className="mt-2 text-lg">Your UK medical cannabis portal.</p>
+      </div>
+    </>
+  );
+}
